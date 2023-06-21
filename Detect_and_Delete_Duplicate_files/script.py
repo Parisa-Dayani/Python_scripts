b23a6cb37ff637ad62b50a75b8b0209b47764347
Parisa-Dayani <83198050+Parisa-Dayani@users.noreply.github.com>
2023-06-21 07:57:57 +0330

    Create script.py

---
+++ Detect_and_Delete_Duplicate_files/script.py
@@ -0,0 +1,20 @@
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+import os
+import hashlib
+
+Tk().withdraw()
+path = askdirectory(title="select a folder")
+
+walker = os.walk(path)
+uniquefiles = dict()
+for folder,sub_folder,files in walker:
+    for file in files:
+        filepath = os.path.join(folder,file)
+        filehash = hashlib.md5(open(filepath,"rb").read()).hexdigest()
+        
+        if filehash in uniquefiles:
+            os.remove(filepath)
+            print(f"{filepath} has been deleted")
+        else:
+            uniquefiles[filehash] = path
