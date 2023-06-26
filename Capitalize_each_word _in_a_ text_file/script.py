41dfbdc72e72f813687e34d3c8ad4b1ce9619fc1
Parisa-Dayani <83198050+Parisa-Dayani@users.noreply.github.com>
2023-06-26 09:44:05 +0330

    Update script.py

--- Capitalize_each_word _in_a_ text_file/script.py
+++ Capitalize_each_word _in_a_ text_file/script.py
@@ -1,15 +1,31 @@
 totContent = ""
-print("Enter the Name of File: ")
+print(end="Enter the Name of File: ")
 fileName = str(input())
-fileHandle = open(fileName, "r")
 
-for content in fileHandle:
-    newContent = content.title()
-    totContent = totContent + newContent
-
-fileHandle.close()
-fileHandle = open(fileName, "w")
-fileHandle.write(totContent)
-
-print("All Words Capitalized Successfully!")
-fileHandle.close()
+try:
+    fileHandle = open(fileName, "r")
+    for content in fileHandle:
+        newContent = content.title()
+        totContent = totContent + newContent
+    fileHandle.close()
+	
+    try:
+        fileHandle = open(fileName, "w")
+        fileHandle.write(totContent)
+        print("\nAll Words in \"" + fileName + "\" Capitalized Successfully!")
+        print(end="\nWant to see the Content of File (y/n): ")
+        ch = input()
+        if ch=='y':
+            fileHandle = open(fileName, "r")
+            for content in fileHandle:
+                print(end=content)
+        else:
+            print("Exiting...")
+        fileHandle.close()
+        print()
+    except IOError:
+        print("Error Occurred!")
+		
+except IOError:
+    print("\nThe \"" + fileName + "\" is not Exist!")
+    print("Exiting...")
